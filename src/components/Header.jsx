20702fb866d2250f621612263f1453fa1ff9cb3e
Anmol-dev45 <basnetanmol2020@gmail.com>
2023-09-11 08:56:34 +0545

    Updated ui

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
             <Container>
                 <div className="flex justify-between items-center">
                     <h1 className=' text-3xl md:text-4xl text-primary font-bold  capitalize font-righteous '>dailyDose nepal</h1>
-                    <nav className={`max-sm:fixed z-20 right-0 top-0 max-sm:h-screen  max-sm:bg-gray-200 max-sm:pt-20  flex max-sm:flex-col max-sm:w-2/3 items-center gap-4 md:gap-12 uppercase ${isOpen ? 'max-sm:translate-x-0' : 'max-sm:translate-x-full'} transition-all duration-300 ease-in-out`}>
+                    <nav className={`max-md:fixed z-20 right-0 top-0 max-md:h-screen  max-md:bg-gray-200 max-md:pt-20  flex max-md:flex-col max-md:w-2/3 items-center gap-4 md:gap-12 uppercase ${isOpen ? 'max-md:translate-x-0' : 'max-md:translate-x-full'} transition-all duration-300 ease-in-out`}>
                         <a href="" className='hover:text-primary transition-colors duration-300 ease-in-out'>home</a>
                         <a href="" className='hover:text-primary transition-colors duration-300 ease-in-out'>about</a>
                         <a href=""><Button>contact</Button></a>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,15 +6,20 @@ const Hero = () => {
     return (
         <section className='mt-20'>
             <Container>
-                <div className="flex flex-col md:flex-row justify-between relative border-3 items-start md:items-end gap-y-16">
+                <div className="flex flex-col-reverse md:flex-row justify-between relative border-3 items-start md:items-end gap-y-6">
+                   
                     <div className='flex-1'>
-                        <h1 className='text-4xl md:text-5xl font-medium font-righteous tracking-wider md:tracking-widest text-primary mb-2 md:mb-4'>Welcome to <br />DailyDose Nepal!!!</h1>
+                        <h1 className='max-md:hidden text-5xl font-medium font-righteous tracking-widest text-primary mb-4'>Welcome to <br />DailyDose Nepal!!!</h1>
                         <p className='font-medium opacity-80 mb-8 md:mb-12 md:pr-8 md:text-left'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam animi aliquid debitis eos. Eius culpa ducimus quidem porro asperiores, quasi, dicta illum tempore deleniti modi atque deserunt provident voluptatum amet molestias accusantium quam facere!</p>
-                        <a href=""><Button>Inform us</Button></a>
+                        <div className='flex gap-4 items-center'>
+                            <a href="https://forms.gle/J7dBXt996fWmkcGo7" target='_blank'><Button>join us</Button></a>
+                            <a href="https://forms.gle/o3hciwwmWe7WFyvx5" target='_blank'><Button>Inform us</Button></a>
+                        </div>
                     </div>
                     <div className="flex-1">
                         <img src="/hero.jpg" alt="" />
                     </div>
+                     <h1 className='md:hidden text-4xl font-medium font-righteous tracking-wider text-primary mb-2'>Welcome to <br />DailyDose Nepal!!!</h1>
                 </div>
             </Container>
         </section>
